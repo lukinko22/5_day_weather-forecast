5367ede246bc82363ba43a6842978a44bd9b13a3
Lukas <lukinko22@yahoo.co.uk>
2023-02-28 11:43:01 +0000

    added 3 hrs forecasts for all 5 days

--- script.js
+++ script.js
@@ -45,3 +45,108 @@ function forecastDiv (day, image, temp, wind, humidity) {
                     </div>`);
     $('#forecast').append(weather);
 }
+// forecast from the API is updated every 3 hours
+function getIndexDay1 (currentHour) {
+    let index = 0
+    if (currentHour >= 0 && currentHour <= 2) {
+        index = 0;   
+    } else if (currentHour >= 3 && currentHour <= 5) {
+        index = 1;        
+    } else if (currentHour >= 6 && currentHour <= 8) {
+        index = 2;
+    } else if (currentHour >= 9 && currentHour <= 11) {
+        index = 3;
+    } else if (currentHour >= 12 && currentHour <= 14) {
+        index = 4;
+    } else if (currentHour >= 15 && currentHour <= 17) {
+        index = 5;
+    } else if (currentHour >= 18 && currentHour <= 20) {
+        index = 6;
+    } else if (currentHour >= 21 && currentHour <= 23) {
+        index = 7;
+    } return index;
+}
+
+function getIndexDay2 (currentHour) {
+    let index = 8
+    if (currentHour >= 0 && currentHour <= 2) {
+        index = 8;   
+    } else if (currentHour >= 3 && currentHour <= 5) {
+        index = 9;        
+    } else if (currentHour >= 6 && currentHour <= 8) {
+        index = 10;
+    } else if (currentHour >= 9 && currentHour <= 11) {
+        index = 11;
+    } else if (currentHour >= 12 && currentHour <= 14) {
+        index = 12;
+    } else if (currentHour >= 15 && currentHour <= 17) {
+        index = 13;
+    } else if (currentHour >= 18 && currentHour <= 20) {
+        index = 14;
+    } else if (currentHour >= 21 && currentHour <= 23) {
+        index = 15;
+    } return index;
+}
+
+function getIndexDay3 (currentHour) {
+    let index = 16
+    if (currentHour >= 0 && currentHour <= 2) {
+        index = 16;   
+    } else if (currentHour >= 3 && currentHour <= 5) {
+        index = 17;        
+    } else if (currentHour >= 6 && currentHour <= 8) {
+        index = 18;
+    } else if (currentHour >= 9 && currentHour <= 11) {
+        index = 19;
+    } else if (currentHour >= 12 && currentHour <= 14) {
+        index = 20;
+    } else if (currentHour >= 15 && currentHour <= 17) {
+        index = 21;
+    } else if (currentHour >= 18 && currentHour <= 20) {
+        index = 22;
+    } else if (currentHour >= 21 && currentHour <= 23) {
+        index = 23;
+    } return index;
+}
+
+function getIndexDay4 (currentHour) {
+    let index = 24
+    if (currentHour >= 0 && currentHour <= 2) {
+        index = 24;   
+    } else if (currentHour >= 3 && currentHour <= 5) {
+        index = 25;        
+    } else if (currentHour >= 6 && currentHour <= 8) {
+        index = 26;
+    } else if (currentHour >= 9 && currentHour <= 11) {
+        index = 27;
+    } else if (currentHour >= 12 && currentHour <= 14) {
+        index = 28;
+    } else if (currentHour >= 15 && currentHour <= 17) {
+        index = 29;
+    } else if (currentHour >= 18 && currentHour <= 20) {
+        index = 30;
+    } else if (currentHour >= 21 && currentHour <= 23) {
+        index = 31;
+    } return index;
+}
+
+function getIndexDay5 (currentHour) {
+    let index = 32
+    if (currentHour >= 0 && currentHour <= 2) {
+        index = 32;   
+    } else if (currentHour >= 3 && currentHour <= 5) {
+        index = 33;        
+    } else if (currentHour >= 6 && currentHour <= 8) {
+        index = 34;
+    } else if (currentHour >= 9 && currentHour <= 11) {
+        index = 35;
+    } else if (currentHour >= 12 && currentHour <= 14) {
+        index = 36;
+    } else if (currentHour >= 15 && currentHour <= 17) {
+        index = 37;
+    } else if (currentHour >= 18 && currentHour <= 20) {
+        index = 38;
+    } else if (currentHour >= 21 && currentHour <= 23) {
+        index = 39;
+    } return index;
+}
